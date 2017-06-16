e0766ea50d1aa025bb4634418ddb4f5f463a3f3b
Ryan Turner <ryanthepit@gmail.com>
2017-06-16 15:19:30 -0700

    blueify method

--- bitmap-bluemangroup/index.js
+++ bitmap-bluemangroup/index.js
@@ -1,33 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const Bitmap = require('./model/bitmap.js')
+const Bitmap = require('./model/bitmap.js');
 
 const filePaths = [
   './assets/bitmap.bmp',
   './assets/finger-print.bmp',
   './assets/house.bmp',
   './assets/newpicpls.bmp',
-  './assets/non-palette-bitmap.bmp'
-]
+  './assets/non-palette-bitmap.bmp',
+];
 
 const parseBitmap = (filePath, callback) =>
   fs.readFile(filePath, (err, data) =>
-    callback(err, data))
+    callback(err, data));
 
 const createBitmap = (filePath, bitmap) => {
-  const path = `${filePath.slice(0, -4)}-${bitmap.transformType}.bmp`
+  const path = `${filePath.slice(0, -4)}-${bitmap.transformType}.bmp`;
   fs.writeFile(path, bitmap.bufferClone, 'utf16le', () => {
-    console.log(path, 'successfully created')
-  })
-}
+    console.log(path, 'successfully created');
+  });
+};
 
-parseBitmap(filePaths[1], (err, data) => {          //parse
-  if (err) return console.log(err)
+parseBitmap(filePaths[3], (err, data) => {          //parse
+  if (err) return console.log(err);
 
   let bitmap = new Bitmap(data);                    //instantiate
 
-  bitmap.inverse()                                  //transform
+  bitmap.blueify();                                 //transform
 
-  createBitmap(filePaths[1], bitmap)                //create
+  createBitmap(filePaths[3], bitmap);                //create
 
-})
+});
--- bitmap-bluemangroup/model/bitmap.js
+++ bitmap-bluemangroup/model/bitmap.js
@@ -1,41 +1,45 @@
 module.exports = class Bitmap {
   constructor(buffer) {
-    this.os = buffer.toString('utf8', 0, 2)
+    this.os = buffer.toString('utf8', 0, 2);
     this.size = buffer.readUInt32LE(2);
     this.offset = buffer.readUInt32LE(10);
-    this.dibHeaderSize = buffer.readUInt32LE(14)
-    this.colorTable = buffer.toString('hex', 58, 186).match(/.{1,8}/g)
+    this.dibHeaderSize = buffer.readUInt32LE(14);
+    this.colorTable = buffer.toString('hex', 58, 186).match(/.{1,8}/g);
     this.bufferClone = buffer;
     this.transformType = '';
   }
 
   printTheBuffer() {
-    console.log(this.bufferClone)
+    console.log(this.bufferClone);
   }
 
   printColorTable() {
-    console.log(this.colorTable)
+    console.log(this.colorTable);
   }
 
   updateColorTable(str) {
-    this.bufferClone.write(str, 58, 128, 'hex')
-  }
-
-  whitewash() {
-    this.transformType = 'whitewashed'
-    this.colorTable = [...this.colorTable].map(v => 'ffffff00')
-    this.updateColorTable(this.colorTable.join(''))
+    this.bufferClone.write(str, 58, 128, 'hex');
   }
 
   inverse() {
-    this.transformType = 'inversed'
+    this.transformType = 'inversed';
     const rgbValues = [...this.colorTable].map(a => {
       return [a.slice(0, 2), a.slice(2,4), a.slice(4, 6)].map(b => {
-        const rgbValue = (Math.abs(255 - parseInt(b, 16))).toString(16)
-        return rgbValue.length === 1 ? `0${rgbValue}` : rgbValue
-      }).join('')
-    })
-    this.updateColorTable(rgbValues.join(''))
+        const rgbValue = (255 - parseInt(b, 16)).toString(16);
+        return rgbValue.length === 1 ? `0${rgbValue}` : rgbValue;
+      }).join('');
+    });
+    this.updateColorTable(rgbValues.join(''));
   }
 
+  blueify(){
+    this.transformType = 'blueified';
+    const rgbValues = [...this.colorTable].map(a => {
+      return [a.slice(0, 2), a.slice(2,4), a.slice(4, 6)].map((b, i) => {
+        let rgb = i === 0 ? 'ff' : Math.floor(parseInt(b,16)/2).toString(16);
+        return rgb.length === 1 ? `0${rgb}` : rgb;
+      }).join('');
+    });
+    this.updateColorTable(rgbValues.join(''));
+  }
 };
