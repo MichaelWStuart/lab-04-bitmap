a0f23025c053058943e311e460e0dc01966c47fc
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-16 09:47:46 -0700

    add inverse method

--- bitmap-bluemangroup/bitmap.js
+++ bitmap-bluemangroup/bitmap.js
@@ -27,4 +27,15 @@ module.exports = class Bitmap {
     this.updateColorTable(this.colorTable.join(''))
   }
 
+  inverse() {
+    this.transformType = 'inversed'
+    const rgbValues = [...this.colorTable].map(a => {
+      return [a.slice(0, 2), a.slice(2,4), a.slice(4, 6)].map(b => {
+        const rgbValue = (Math.abs(255 - parseInt(b, 16))).toString(16)
+        return rgbValue.length === 1 ? `0${rgbValue}` : rgbValue
+      }).join('')
+    })
+    this.updateColorTable(rgbValues.join(''))
+  }
+
 };
--- bitmap-bluemangroup/index.js
+++ bitmap-bluemangroup/index.js
@@ -21,13 +21,13 @@ const createBitmap = (filePath, bitmap) => {
   })
 }
 
-parseBitmap(filePaths[0], (err, data) => {          //parse
+parseBitmap(filePaths[1], (err, data) => {          //parse
   if (err) return console.log(err)
 
   let bitmap = new Bitmap(data);                    //instantiate
 
-  bitmap.whitewash()                                //transform
+  bitmap.inverse()                                  //transform
 
-  createBitmap(filePaths[0], bitmap)                //create
+  createBitmap(filePaths[1], bitmap)                //create
 
 })
