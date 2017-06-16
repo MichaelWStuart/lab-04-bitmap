a69863fd554b02126e213a9260db59cf833a7610
derekcfarmer <derek.farmer@gmail.com>
2017-06-15 17:06:15 -0700

    constructor finished

--- bitmap-bluemangroup/lib/file-ops.js
+++ bitmap-bluemangroup/lib/file-ops.js
@@ -12,14 +12,14 @@ const files = [
   './assets/non-palette-bitmap.bmp'
 ]
 
-const display = data => console.log(data)
+// const display = data => console.log(data)
 
-readBitmap.getFileHeader = (file, cb) => fs.readFile(file, (err, data) => cb(data.slice(0, 14)))
+readBitmap.getFileHeader = (file) => fs.readFile(file, (err, data) => data.slice(0, 14)))
 
-readBitmap.getDIBHeader = (file, cb) => fs.readFile(file, (err, data) => data.slice(15, 54))
+readBitmap.getDIBHeader = (file) => fs.readFile(file, (err, data) => data.slice(15, 54))
 
-readBitmap.getColorTable = (file, cb) => fs.readFile(file, (err, data) => data.slice(55, 1078))
+readBitmap.getColorTable = (file) => fs.readFile(file, (err, data) => data.slice(55, 1078))
 
-readBitmap.getPixelArray = (file, cb) => fs.readFile(file, (err, data) => data.slice(1079))
+readBitmap.getPixelArray = (file) => fs.readFile(file, (err, data) => data.slice(1079))
 
-readBitmap.getFileHeader(files[0], display)
+// readBitmap.getFileHeader(files[0], display)
